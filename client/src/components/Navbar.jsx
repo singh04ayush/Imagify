c40f9d3e97637fdb6205e26a40de62da9de1ce0a
Ayush Singh <2023505161.ayush@ug.sharda.ac.in>
2025-03-15 15:58:40 +0000

    Header

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import {assets} from '../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
 
-  const [user, setUser] = useState(null);
+  const {user} = useContext(AppContext)
 
   const navigate = useNavigate();
 
@@ -17,7 +18,21 @@ const Navbar = () => {
       <div>
         {
         user ?
-        <div></div>
+        <div className='flex items-centre gap-2 sm-gap-3'>
+          <button onClick={()=>navigate('/buy')} className='flex items-centre gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700'>
+            <img className='w-5' src={assets.credit_star} alt="" />
+            <p className='text-xs sm:text-sm font-medium text-grey-600'>Credit left : 50</p>
+          </button>
+          <p className='text-grey-600 max-sm:hidden pl-4'>Hi, Ayush</p>
+          <div className='relative group'>
+            <img src={assets.profile_icon} className='w-10 drop-shadow' alt="" />
+            <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+              <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
+              </ul>
+            </div>
+          </div>
+        </div>
         :
         <div className='flex items-centre gap-2 sm:gap-5'>
           <p onClick={()=>navigate('/buy')} className='cursor-pointer'>Pricing</p>
