aca6254b4811b76142140fffe492c1a19dda11e1
Ayush Singh <sayhi.ayush@gmail.com>
2025-03-23 23:51:32 +0530

    new init

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       
       <img src={assets.logo} alt="" width={150} />
 
-      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @AyushSingh | All right reserved.</p>
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @AyushSingh | All right reserved</p>
 
       <div className='flex gap-2.5'>
         <img src={assets.facebook_icon} alt="" width={35} />
