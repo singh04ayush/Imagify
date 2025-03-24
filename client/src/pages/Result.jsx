db65c29908a6f1f539ec189cb02fbbbaa4d2cb87
Ayush Singh <sayhi.ayush@gmail.com>
2025-03-24 23:51:53 +0530

    working on result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Result = () => {
   return (
     <div>
-      result
+      <div className='relative'>
+        <img src={assets.sample_img_1} alt="" className='max-w-sm rounded' />
+        <span className='absolute bottom-0 h-1 bg-blue-500 w-full transistion-all duration-[10]'></span>
+      </div>
+      <p>Loading...</p>
     </div>
   )
 }
