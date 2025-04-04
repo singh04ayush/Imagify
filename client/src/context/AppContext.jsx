1946554677ad09859b6e20727e708ca8bb524663
Nishantsingh11 <ns114046@gmail.com>
2025-04-05 00:50:34 +0530

    Logout problem #8

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,10 +2,9 @@ import React, { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
-
 export const AppContext = createContext();
 
-const AppContextProvider = (props) =>{
+const AppContextProvider = (props) => {
     const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false);
     const [token, setToken] = useState(localStorage.getItem('token'))
@@ -16,11 +15,11 @@ const AppContextProvider = (props) =>{
 
     const navigate = useNavigate()
 
-    const loadCreditsData = async()=>{
+    const loadCreditsData = async () => {
         try {
-            const {data} = await axios.get(backendUrl + '/api/user/credits', {headers: {token}})
+            const { data } = await axios.get(backendUrl + '/api/user/credits', { headers: { token } })
 
-            if(data.success){
+            if (data.success) {
                 setCredit(data.credits)
                 setUser(data.user)
             }
@@ -31,17 +30,17 @@ const AppContextProvider = (props) =>{
     }
 
 
-    const generateImage = async (prompt)=>{
+    const generateImage = async (prompt) => {
         try {
-            const {data} = await axios.post(backendUrl + '/api/image/generate-image', {prompt}, {headers: {token}})
+            const { data } = await axios.post(backendUrl + '/api/image/generate-image', { prompt }, { headers: { token } })
 
-            if(data.success){
+            if (data.success) {
                 loadCreditsData()
                 return data.resultImage
-            }else{
+            } else {
                 toast.error(data.message)
                 loadCreditsData()
-                if(data.creditBalance === 0){
+                if (data.creditBalance === 0) {
                     navigate('/buy')
                 }
             }
@@ -50,14 +49,17 @@ const AppContextProvider = (props) =>{
         }
     }
 
-    const logout = () =>{
+    const logout = () => {
         localStorage.removeItem('token')
         setToken('')
         setUser(null)
+        navigate('/')
+
+
     }
 
-    useEffect(()=>{
-        if(token){
+    useEffect(() => {
+        if (token) {
             loadCreditsData()
         }
     }, [token])
@@ -65,7 +67,7 @@ const AppContextProvider = (props) =>{
     const value = {
         user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit, loadCreditsData, logout, generateImage
     }
-   
+
     return (
         <AppContext.Provider value={value}>
             {props.children}
