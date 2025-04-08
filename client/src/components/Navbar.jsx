022006f2d968c254d1d86afbd1f0cadf04fe0a3b
Ayush Singh <sayhi.ayush@gmail.com>
2025-04-09 00:57:05 +0530

    fine create btn redirect corrected

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
                         {user ? (
                             <div className="flex items-center space-x-4">
                                 <button 
-                                    onClick={() => navigate('/create')}
+                                    onClick={() => navigate('/result')}
                                     className="hidden sm:flex items-center space-x-2 px-4 py-2 bg-blue-50/50 text-blue-600 rounded-full hover:bg-blue-100/50 transition-colors duration-200"
                                     aria-label="Create new image"
                                 >
--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -8,7 +8,7 @@ const Gallery = () => {
       whileInView={{opacity:1, y:0}}
       viewport={{once:true}} className='min-h-[80vh] text-center pt-14 mb-10'>
 
-      <h1 className='text-center text-3xl font-medium mb-12 sm:mb-6'>Features are on the way..Stay Tuned</h1>
+      <h1 className='text-center text-3xl font-medium mb-12 sm:mb-6'>We're Crafting Magic..Stay Tuned</h1>
       <br />
       <h1 className='text-center text-3xl font-medium mb-12 sm:mb-6'>Coming Soon</h1>
       
